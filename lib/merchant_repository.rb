cb5822c680643ae2a30e99f8bd1973bea34d8c0b
Jon Kimble <jonathanbkimble@gmail.com>
2017-05-10 11:26:27 -0600

    Import csv and turn into merchant objects

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,2 +1,20 @@
+require 'pry'
+require 'csv'
+require './lib/merchant.rb'
+
 class MerchantRepository
+  attr_reader :merchants
+
+  def initialize(file_name)
+    @merchants = create_merchants(file_name)
+  end
+
+  def create_merchants(file_name)
+    merchants = []
+
+    CSV.foreach(file_name, :headers => true) do |row|
+      merchants << Merchant.new(row)
+    end
+    merchants
+  end
 end
