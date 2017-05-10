328200e20dd9553ce449e64e8a2341e18f59056f
Jon Kimble <jonathanbkimble@gmail.com>
2017-05-10 12:04:17 -0600

    Remove comments

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -5,12 +5,6 @@ class SalesEngine
 
   def initialize(csv_files)
     @merchants = create_merchants(csv_files[:merchants])
-
-    # extract file name from csv_files hash
-    # Create an object of merchant repository
-    # merchant repo object loads file and Creates merchant objects
-    # from each line of the csv
-
   end
 
   def self.from_csv(csv_files)
