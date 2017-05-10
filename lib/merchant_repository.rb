4164a18bdb42dd862437528311172f1e845c6884
Jon Kimble <jonathanbkimble@gmail.com>
2017-05-10 12:04:07 -0600

    Add data methods

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -17,4 +17,31 @@ class MerchantRepository
     end
     merchants
   end
+
+  def all
+    merchants
+  end
+
+  def find_by_name(name)
+    merchants.each do |merchant|
+      return merchant if merchant.name.upcase == name.upcase
+    end
+    nil
+  end
+
+  def find_by_id(id)
+    merchants.each do |merchant|
+      return merchant if merchant.id == id
+    end
+    nil
+  end
+
+  def find_all_by_name(name_fragment)
+    final = []
+    merchants.each do |merchant|
+      final << merchant if merchant.name.upcase.include?(name_fragment.upcase)
+    end
+    final
+  end
+
 end
