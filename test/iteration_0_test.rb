bd99409f84991938ea6262e5d320f5ab21c93fb2
Jon Kimble <jonathanbkimble@gmail.com>
2017-05-10 11:28:06 -0600

    Add test for merchants"

--- test/iteration_0_test.rb
+++ test/iteration_0_test.rb
@@ -1,11 +1,12 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/sales_engine.rb'
+require 'pry'
 
 class SalesEngineTest < Minitest::Test
 
   def test_class_exists
-    s = SalesEngine.new
+    s = SalesEngine.new({:merchants => "./data/merchants.csv"})
     assert_equal SalesEngine, s.class
   end
 
@@ -14,7 +15,30 @@ class SalesEngineTest < Minitest::Test
       :merchants => "./data/merchants.csv"
       })
 
-    assert_equal MerchantRepository, se.merchants.class
+    assert_equal SalesEngine, se.class
+  end
+
+  def test_can_create_merchant_repository
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+    })
+
+    mr = se.merchants
+    assert_equal mr.class, MerchantRepository
+  end
+
+  def test_it_creates_merchant_objects
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+    })
+
+    mr = se.merchants
+    assert_equal mr.merchants.first.class, Merchant
+    assert_equal mr.merchants.length, 475
+    assert_equal mr.merchants.first.id, "12334105"
+    assert_equal mr.merchants.first.name, "Shopin1901"
   end
 
 end
