378624630aadc3a63055aa25096a333bea2e796d
Jon Kimble <jonathanbkimble@gmail.com>
2017-05-10 11:27:29 -0600

    Add create merchants method

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,24 +1,25 @@
 require './lib/merchant_repository.rb'
 
 class SalesEngine
+  attr_reader :merchants
 
-  def self.from_csv(csv_hash)
-    csv_hash.each do |key, value|
-      if key == :merchants
-        #extract this to a different class and intantiate object
-        # of said class here to start upload process.
-        import_merchants(value)
-      end
-    end
-    # Creates an object based on the key in the hash
-    # object contains all csv rows as individual objects
+  def initialize(csv_files)
+    @merchants = create_merchants(csv_files[:merchants])
 
-
+    # extract file name from csv_files hash
+    # Create an object of merchant repository
+    # merchant repo object loads file and Creates merchant objects
+    # from each line of the csv
 
   end
 
-  def import_merchants(value)
-    repo = MerchantRepository.new
+  def self.from_csv(csv_files)
+    new(csv_files)
   end
+
+  def create_merchants(file_name)
+    MerchantRepository.new(file_name)
+  end
+
 
 end
