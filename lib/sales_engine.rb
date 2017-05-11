fedbb34c50ef4aacbe330f325b5b40265d66a070
Nathan Brickett <Nathan Brickett>
2017-05-11 13:56:49 -0400

    added item repository

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,10 +1,15 @@
 require './lib/merchant_repository.rb'
+require './lib/item_repository.rb'
+require 'pry'
 
 class SalesEngine
-  attr_reader :merchants
+
+  attr_reader :merchants, :items
 
   def initialize(csv_files)
+  
     @merchants = create_merchants(csv_files[:merchants])
+    @items = create_items(csv_files[:items])
   end
 
   def self.from_csv(csv_files)
@@ -15,5 +20,8 @@ class SalesEngine
     MerchantRepository.new(file_name)
   end
 
+  def create_items(file_name)
 
+    ItemRepository.new(file_name)
+  end
 end
