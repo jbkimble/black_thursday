90cc101c7f97f5c775a135f899d837bd66aedb8e
Nathan Brickett <Nathan Brickett>
2017-05-11 14:00:42 -0400

    created item object with attributes from csv

---
+++ lib/item.rb
@@ -0,0 +1,18 @@
+class Item
+  attr_reader :id, :name, :description, :unit_price,
+              :created_at, :updated_at, :merchant_id
+
+  def initialize(row)
+    @id = row["id"]
+    @name = row["name"]
+    @description = row["description"]
+    @unit_price = row["unit_price"]
+    @created_at = row["created_at"]
+    @updated_at = row["updated_at"]
+    @merchant_id = row["merchant_id"]
+  end
+
+  def unit_price_to_dollars
+    unit_price.to_f
+  end
+end
