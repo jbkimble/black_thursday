d0e5e46474a8f2ebb47413c22514e4f901f04553
Nathan Brickett <Nathan Brickett>
2017-05-15 21:06:45 -0400

    added tests for merchant analytic methods

--- test/merchant_analytics_test.rb
+++ test/merchant_analytics_test.rb
@@ -29,4 +29,37 @@ class MerchantAnalyticsTest < Minitest::Test
     assert_equal sa.top_revenue_earners(5).first, "Keckenbauer"#=> [merchant, merchant, merchant, merchant, merchant]
     assert_equal sa.top_revenue_earners.count, 20
   end
+
+  def test_method_merchants_with_pending_invoices_returns_merchants_with_invoices_pending
+    sa = SalesAnalyst.new(se)
+    assert_equal sa.merchants_with_pending_invoices.count, 385 #=> [merchant, merchant, merchant]
+    #Note: an invoice is considered pending if none of its transactions are successful.
+  end
+
+  def test_method_merchants_with_only_one_item_returns_merchants_with_one_item
+    sa = SalesAnalyst.new(se)
+    assert_equal sa.merchants_with_only_one_item.count, 243 #=> [merchant, merchant, merchant]
+  end
+
+  def test_method_merchants_with_only_one_item_registered_in_month_returns_merchants_that_only_registered_one_item_in_given_month
+    sa = SalesAnalyst.new(se)
+    assert_equal sa.merchants_with_only_one_item_registered_in_month("January").count, 19 #=> [merchant, merchant, merchant]
+  end
+
+  def test_method_revenue_per_merchant_returns_the_revenue_in_dollars_for_corresponding_merchant_id
+    sa = SalesAnalyst.new(se)
+    assert_equal sa.revenue_by_merchant("12334112"), "$519026" #=> $
+  end
+
+  def test_method_most_sold_item_for_merchant_returns_item_that_sold_the_most
+    sa = SalesAnalyst.new(se)
+    assert_equal sa.most_sold_item_for_merchant("12334105"), ["Vogue Paris Original Givenchy 2307"] #=> [item] (in terms of quantity sold) or, if there is a tie, [item, item, item]
+  end
+
+  def test_method_best_item_for_merchant_returns_the_item_that_has_produced_the_most_revenue
+    sa = SalesAnalyst.new(se)
+    assert_equal sa.best_item_for_merchant("12334113"), ["Custom Hand Made Miniature Bicycle"] #=> item (in terms of revenue generated)
+  end
+
+
 end
